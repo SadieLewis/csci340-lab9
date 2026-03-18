2864a0a3bb31a72f72889d5d9f2af5419abbdd11
SadieLewis <you@example.com>
2026-03-17 23:08:05 -0500

    Sort, Filter, Page, and Group

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b0d41f837404c85940be7bedaf16d08b913cf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f965eca54e1483458cbe5d0f21283a53acf3015")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
